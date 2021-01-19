a455544830bc38cc4c962c2316f99ccb339b26d4
Yosh11 <isok.1997@mail.ru>
2021-01-19 22:13:10 +0300

    feat add migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/Yosh11/exemple_gin/pkg/handler"
 	"github.com/Yosh11/exemple_gin/pkg/repository"
 	"github.com/Yosh11/exemple_gin/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(web.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while starting server: %s ", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
