8fbd0cf6d2e9982e5d97ed5e72ae654c5a58a076
Yosh11 <isok.1997@mail.ru>
2021-02-03 03:43:27 +0300

    chore graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Yosh11/exemple_gin/model/todo"
 	"github.com/Yosh11/exemple_gin/pkg/handler"
 	"github.com/Yosh11/exemple_gin/pkg/repository"
 	"github.com/Yosh11/exemple_gin/pkg/service"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq" // ...
 	log "github.com/sirupsen/logrus"
@@ -40,13 +45,41 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occurred while starting server: %s ", err.Error())
-	}
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			log.Errorf("error occurred while starting server: %s", err.Error())
+		}
+	}()
+
+	log.Infoln("TodoApp Started")
+
+	// Graceful shutdown server and db with timeout
+	graceful(srv, db, 5*time.Second)
 }
 
 func initConfig() error {
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
+}
+
+func graceful(srv *todo.Server, db *sqlx.DB, timeout time.Duration) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := db.Close(); err != nil {
+		log.Errorf("error occurred on db connection close: %s", err.Error())
+	} else {
+		log.Infoln("db closed")
+	}
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Errorf("error occurred on server shutting down: %s", err.Error())
+	} else {
+		log.Infof("TodoApp ShutDown with timeout: %v", timeout)
+	}
 }
